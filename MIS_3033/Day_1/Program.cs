4d7e095a293ead300d5d4e854344d6a66121dc0f
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-01-19 11:48:26 -0600

    Day_1 Runthrough

---
+++ MIS_3033/Day_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Print the welcome messsage 
+            /*
+             This is our first class 
+             
+             */
+            string name;
+
+            
+
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+            string welcome =string.Format( $" Hi, {name},Welcome to MIS 3033");
+            Console.WriteLine(welcome);
+        }
+    }
+}
