4f9e7aee5a32f1e423f5e78e09ad93401687e60e
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-01-24 11:00:03 -0600

    Day_2Code

---
+++ MIS3033_2/Day_2Code/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day_2Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to MIS 3033!");
+            Console.ReadKey();
+        }
+    }
+}
