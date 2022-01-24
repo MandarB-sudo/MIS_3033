aee36834b26c97b62b74b34d86baaaec9195fb1c
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-01-24 11:44:46 -0600

    Hw1_Task1

---
+++ Mandar_Bornare_HW1.zip/Mandar_Bornare_HW1.zip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mandar_Bornare_HW1.zip
+{
+    class Program
+    {
+        const double whole_sale_cogs_price = 79.99;
+        const int whole_sale_gears_price = 250;
+        const double normalmarkup = 0.15;
+        const double discount_markup = 0.12;
+        const double sales_tax = 0.089;
+        static void Main(string[] args)
+        {
+            
+            int cogs_quantity , gears_quanitity;
+            double sales_total, tax, discount,Total;
+            Console.WriteLine("Please input the number of Cogs you would like to buy");
+            string ans1 = Console.ReadLine();
+            cogs_quantity = Convert.ToInt32(ans1);
+            Console.WriteLine("Please input the number of Gears you would like to buy");
+            string ans2= Console.ReadLine();
+            gears_quanitity = Convert.ToInt32(ans2);
+
+
+
+        }
+    }
+}
