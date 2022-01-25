4b200dd67f34619a4a96459323addb2a0714b426
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-01-25 16:46:14 -0600

    Task2 completed

---
+++ Mandar_Bornare_HW1.zip/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = 0, sum = 0;
+            string ans;
+            for (int i = 1; i<=3; i++)
+            {
+                Console.WriteLine("Please enter your exam score");
+                 ans = Console.ReadLine();
+                grade = Convert.ToDouble(ans);
+                sum += grade;
+            }
+            
+            double avg = ((sum/ 3)/100);
+            Console.WriteLine($"The percentage you have is {avg.ToString("P")}");
+        }
+    }
+}
