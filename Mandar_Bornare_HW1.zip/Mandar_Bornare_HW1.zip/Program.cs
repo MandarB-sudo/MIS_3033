4e28c148e41efff47f507377839b46ff1409924e
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-01-25 16:24:04 -0600

    Part of task 1 done

--- Mandar_Bornare_HW1.zip/Mandar_Bornare_HW1.zip/Program.cs
+++ Mandar_Bornare_HW1.zip/Mandar_Bornare_HW1.zip/Program.cs
@@ -11,9 +11,9 @@ namespace Mandar_Bornare_HW1.zip
         const double sales_tax = 0.089;
         static void Main(string[] args)
         {
-            
-            int cogs_quantity , gears_quanitity;
-            double sales_total, tax, discount,Total;
+
+            int cogs_quantity, gears_quanitity, Totalquantity = 0 ;
+            double sales_total, sales_markup, tax, discount,Total;
             Console.WriteLine("Please input the number of Cogs you would like to buy");
             string ans1 = Console.ReadLine();
             cogs_quantity = Convert.ToInt32(ans1);
@@ -21,7 +21,24 @@ namespace Mandar_Bornare_HW1.zip
             string ans2= Console.ReadLine();
             gears_quanitity = Convert.ToInt32(ans2);
 
+            Totalquantity = cogs_quantity + gears_quanitity;
 
+            if (Totalquantity < 16 && cogs_quantity <= 10 && gears_quanitity <= 10)
+            {
+                sales_total = (cogs_quantity * whole_sale_cogs_price) + ((cogs_quantity * whole_sale_cogs_price) * normalmarkup) + (gears_quanitity * whole_sale_gears_price)
+                    + ((gears_quanitity * whole_sale_gears_price) * normalmarkup);
+
+            }
+            else if (Totalquantity < 16 && cogs_quantity > 10 && gears_quanitity <= 10)
+            {
+                sales_total = (cogs_quantity * whole_sale_cogs_price) + ((cogs_quantity * whole_sale_cogs_price) * discount_markup) + (gears_quanitity * whole_sale_gears_price)
+                    + ((gears_quanitity * whole_sale_gears_price) * normalmarkup);
+            }
+            else if (Totalquantity < 16 && cogs_quantity <=10 && gears_quanitity >10)
+            {
+                sales_total = (cogs_quantity * whole_sale_cogs_price) + ((cogs_quantity * whole_sale_cogs_price) * normalmarkup) + (gears_quanitity * whole_sale_gears_price)
+                    + ((gears_quanitity * whole_sale_gears_price) * discount_markup);
+            }
 
         }
     }
