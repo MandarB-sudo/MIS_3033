922778e386f9542e4dd599cc988e8f5527b456c3
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-01-26 11:46:04 -0600

    Task 3 started

---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> grades = new List<double>();
+
+
+
+        }
+    }
+}
