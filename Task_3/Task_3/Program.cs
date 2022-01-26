53290d8e70ce25d88c29bf291e07b5ed2fb67565
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-01-26 12:25:16 -0600

    Task 3 in progress

--- Task_3/Task_3/Program.cs
+++ Task_3/Task_3/Program.cs
@@ -8,9 +8,28 @@ namespace Task_3
         static void Main(string[] args)
         {
             List<double> grades = new List<double>();
+            string ans;
+            double grade , avg, sum = 0;
+            do
+            {
 
+            
+            Console.WriteLine("Please enter your exam score ");
+                ans = Console.ReadLine();
+                grade = Convert.ToDouble(ans);
+                grades.Add(grade);
 
+                Console.WriteLine("Would you like to enter another score ?");
+                ans = Console.ReadLine();
+            } while (ans.ToUpper() == "YES");
 
+            for (int i = 0; i < grades.Count; i++)
+            {
+                sum+= grades[i];
+            }
+            avg = sum / grades.Count;
+
+            Console.WriteLine(avg);
         }
     }
 }
