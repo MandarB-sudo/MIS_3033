6455421534f61badb2e5c77626016ba0c14b3dbc
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-02-03 15:02:51 -0600

    Exercise 1 almost done

---
+++ Lecture_Collections/Lecture_Collections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stuNames = new string[5];
+            int[] stuAge = new int[5];
+            double[] StudGradedbl = { 95.5, 90, 90.6, 94, 100 };
+            stuNames[0] = "Mandar";
+            stuNames[1] = "James";
+            stuNames[2] = "Rita";
+            stuNames[3] = "Cory";
+            stuNames[4] = "Zack";
+
+            //var numberofStu = stuNames.Length;
+            //Console.WriteLine(stuNames.Length);
+            //Array.Reverse(stuNames);
+            //Array.Sort(stuNames);
+            //Array.Sort(StudGradedbl);
+
+            //string[] stunamescopy = new string[5];
+            //Array.Copy(stuNames, stunamescopy, 5);
+
+            //stunamescopy[4] = "Change Name";
+
+            List<string> StuNamesList = new List<string>();
+            StuNamesList.Add("Max");
+            StuNamesList.AddRange(stuNames);
+            int InList = StuNamesList.Count;
+            
+
+
+
+        }
+    }
+}
---
+++ Student_Names/Student_Names/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Student_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ListofStudents = new List<string>();
+            string userinput;
+            int lenghtofname = 0;
+            double avg = 0;
+            Console.WriteLine("Please enter the name of the 35 students or enter -1 to print the names already mentioned");
+
+            do
+            {
+
+     
+                Console.WriteLine("Please enter the name of Student ");
+                userinput = Console.ReadLine();
+                ListofStudents.Add(userinput);
+        } while (ListofStudents.Count <=35 && userinput!= "-1");
+                
+            
+               
+
+
+
+
+
+           
+            
+            foreach (var student in ListofStudents)
+            {
+              
+                Console.WriteLine(student);
+            }
+            Console.WriteLine($"The number of students is {ListofStudents.Count}");
+
+            for (int i = 0; i < ListofStudents.Count; i++)
+            {
+                 lenghtofname+= ListofStudents[i].Length;
+                
+                   
+            }
+            avg = lenghtofname / ListofStudents.Count;
+            Console.WriteLine($"The avg lenght of name is {avg}");
+    
+        }
+    }
+}
