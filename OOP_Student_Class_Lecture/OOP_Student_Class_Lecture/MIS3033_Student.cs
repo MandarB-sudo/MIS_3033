852187efcfe11baae13d0d81bd199bc998997b07
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2022-02-07 11:25:39 -0600

    C# Review of classes and function

---
+++ OOP_Student_Class_Lecture/OOP_Student_Class_Lecture/MIS3033_Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS3033_Student
+{
+    public class Student 
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Id { get; set; }
+        public double  Gpa { get; set; }
+
+        public Student( string f,string l, string id)
+        {
+            this.Firstname = f;
+
+            this.Lastname = l;
+           this.Id = Id;
+        }
+        public override string ToString()
+        {
+            string message = string.Format($" name :{Firstname} {Lastname}, id: {Id}");
+            return message;
+        }
+        public static char Getdoublegrade(double grade)
+        {
+            if (grade>= 90)
+            {
+                return 'A';
+            }
+            else if (grade >= 80)
+            {
+                return 'B';
+            }
+            else if (grade >=70)
+            {
+                return 'C';
+
+            }
+            else if (grade >=60)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+            
+        }
+    }
+}
+
---
+++ OOP_Student_Class_Lecture/OOP_Student_Class_Lecture/Program.cs
@@ -0,0 +1,21 @@
+﻿using MIS3033_Student;
+using System;
+
+namespace OOP_Student_Class_Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student mandarstu = new Student("Mandar", "Bornare", "11345679");
+            string stuInfo = mandarstu.ToString();
+
+            Console.WriteLine("Input a grade");
+            double g = Convert.ToDouble(Console.ReadLine());
+            double weight = 150.6;
+            string message = weight.ToString();
+             char lg = mandarstu.Getdoublegrade(g);
+            Console.WriteLine(lg);
+        }
+    }
+}
